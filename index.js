5dc9e3f784c78a67e636f2a3b4d4685a3cd5d89d
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-06 21:18:42 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+
+Return the number of strings in words that are a prefix of s.
+
+A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+
+
+
+*/
+
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {number}
+ */
+var countPrefixes = function(words, s) {
+    return words.filter(a => a === s.split('').slice(0, a.length).join('')).length;
+};
